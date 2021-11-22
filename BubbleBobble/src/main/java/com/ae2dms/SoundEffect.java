5f5928bf0019437f8d8306e47b41a56ea7659c05
Zack Hu <scyzh6@nottingham.edu.cn>
2021-11-22 15:03:26 +0800

    refactor: rewrite soundeffect with javafx

--- BubbleBobble/src/main/java/com/ae2dms/SoundEffect.java
+++ BubbleBobble/src/main/java/com/ae2dms/SoundEffect.java
@@ -1,8 +1,10 @@
 package com.ae2dms;
 
-import java.io.*;
-import java.net.URL;
-import javax.sound.sampled.*;
+//import java.io.*;
+//import java.net.URL;
+//import javax.sound.sampled.*;
+
+import javafx.scene.media.AudioClip;
 
 /**
  * SoundEffect handles the game's SFX.
@@ -10,14 +12,14 @@ import javax.sound.sampled.*;
  * play them via the play() method.
  */
 public enum SoundEffect {
-	FRUIT("sfx/fruit.wav"),
-	DEATH("sfx/death.wav"),
-	SHOOT("sfx/shoot.wav"),
-	POP("sfx/pop.wav"),
-	BUBBLED("sfx/bubbled.wav"),
-	JUMP("sfx/jump.wav"),
-	EXPLODE("sfx/explode.wav"),
-	LAND("sfx/land.wav");
+	FRUIT("/sound/fruit.wav"),
+	DEATH("/sound/death.wav"),
+	SHOOT("/sound/shoot.wav"),
+	POP("/sound/pop.wav"),
+	BUBBLED("/sound/bubbled.wav"),
+	JUMP("/sound/jump.wav"),
+	EXPLODE("/sound/explode.wav"),
+	LAND("/sound/land.wav");
 	
 	public static enum Volume {
 		MUTE, LOW, MEDIUM, HIGH
@@ -25,38 +27,45 @@ public enum SoundEffect {
 	
 	public static Volume volume = Volume.LOW;
 	
-	private Clip clip;
+//	private Clip clip;
+	private AudioClip audioClip;
 	
-	SoundEffect(String soundFileName) {
+	SoundEffect(String src) {
 		// sets the sound effect
-		try {
-			URL url = this.getClass().getClassLoader().getResource(soundFileName);
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
-			clip = AudioSystem.getClip();
-			clip.open(audioInputStream);
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			URL url = this.getClass().getClassLoader().getResource(soundFileName);
+//			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
+//			clip = AudioSystem.getClip();
+//			clip.open(audioInputStream);
+//		} catch (UnsupportedAudioFileException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (LineUnavailableException e) {
+//			e.printStackTrace();
+//		}
+
 	}
 	
 	public void play() {
 		// plays the sound effect
 		if (volume != Volume.MUTE) {
-			if (clip.isRunning()) {
-				clip.stop();
+			if (audioClip.isPlaying()) {
+				audioClip.stop();
 			}
-			clip.setFramePosition(0);
-			clip.start();
+			audioClip.play();
+//			if (clip.isRunning()) {
+//				clip.stop();
+//			}
+//			clip.setFramePosition(0);
+//			clip.start();
 		}
 	}
 	
 	public void setToLoop() {
 		// sets whether or not the sound effect loops
-		clip.loop(clip.LOOP_CONTINUOUSLY);
+//		clip.loop(clip.LOOP_CONTINUOUSLY);
+		audioClip.setCycleCount(AudioClip.INDEFINITE);
 	}
 	
 	public void setToLoud() {
