1b403a49bbe0b08718c9bdbf60e32f94303a8f09
Zack Hu <scyzh6@nottingham.edu.cn>
2021-11-23 13:43:09 +0800

    fix: fix the bug that main page can't display
    Modify/refactor the following java files to make it display normally:
     - GamePanel.java
     - Main.java
     - InteractableWorld.java

--- BubbleBobble/src/main/java/com/ae2dms/GamePanel.java
+++ BubbleBobble/src/main/java/com/ae2dms/GamePanel.java
@@ -1,13 +1,15 @@
-package com.ae2dms.UI;
+package com.ae2dms;
 
+import com.ae2dms.GameObject.Objects.Hero;
+import com.ae2dms.GameScene.InteractableWorld;
 import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 /**
  * The GamePanel is where the entire game is constantly updated.
@@ -35,23 +37,72 @@ import javax.swing.Timer;
 //	}
 //}
 
-public class GamePanel extends Canvas {
-	InteractableWorld world;
 
-	public GamePanel(int width, int height) {
-		world = new InteractableWorld(width, height);
 
-//		this.world = world;
+public class GamePanel {
+
+	public static final int UNIT_SIZE = 20;
+	public static final int WIDTH = 40, HEIGHT = 34;
+	private Stage stage;
+
+//	private Canvas canvas = new Canvas((double)UNIT_SIZE * WIDTH, (double)UNIT_SIZE * HEIGHT);
+//	private Hero hero = new Hero();
+
+	private static GamePanel instance = new GamePanel();
+	private InteractableWorld world = new InteractableWorld(UNIT_SIZE * WIDTH, UNIT_SIZE * HEIGHT);
+	private GraphicsContext graphicsContext = world.getGraphicsContext2D();
+
+	GamePanel() {}
+
+	public static GamePanel getInstance() { return instance; }
+
+	public void init(Stage stage) {
+		this.stage = stage;
+		AnchorPane root = new AnchorPane(world);
+		Scene scene = new Scene(root, UNIT_SIZE * WIDTH, UNIT_SIZE * HEIGHT);
+
+		stage.setTitle("Bubble Bobble");
+		stage.getIcons().add(new Image("/icon.jpg"));
+		stage.setResizable(false);
+		stage.setScene(scene);
 		world.startGame();
-		GraphicsContext gc = this.getGraphicsContext2D();
 
+		stage.show();
 
 		new AnimationTimer() {
+			private long lastUpdate = 0 ;
 			public void handle(long currentTime) {
-				world.updatePosition();
 
+//				world.clearRect(0, 0, an)
+				world.updatePosition();
+				world.paintComponent(graphicsContext);
 			}
 		}.start();
-
 	}
+
+	public void gameStart() {
+//		world.
+	}
+
+	public void toIndex() {}
+	public void gameOver() {}
+
+
+
+//	public GamePanel(int width, int height) {
+//		world = new InteractableWorld(width, height);
+//
+////		this.world = world;
+//		world.startGame();
+////		GraphicsContext gc = this.getGraphicsContext2D();
+//
+//
+//		new AnimationTimer() {
+//			public void handle(long currentTime) {
+//				world.updatePosition();
+//
+//			}
+//		}.start();
+//
+//	}
 }
--- BubbleBobble/src/main/java/com/ae2dms/Main.java
+++ BubbleBobble/src/main/java/com/ae2dms/Main.java
@@ -1,44 +1,51 @@
 package com.ae2dms;
 
-import com.ae2dms.UI.GamePanel;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 
-//import javax.swing.JFrame;
 
 /**
- * Main creates a JFrame and adds a com.ae2dms.UI.GamePanel to that frame.
+ * Main creates a JFrame and adds a com.ae2dms.GamePanel to that frame.
  * The size of the GamePanel is determined here.
  */
-public class Main extends Application {
-	public static final int UNIT_SIZE = 20;
-	static final int WIDTH = 40;
-	static final int HEIGHT = 34;
 
+public class Main extends Application {
 	public static void main(String[] args) {
-//		JFrame frame = new JFrame();
-//		frame.setResizable(false);
-//		frame.add(new GamePanel(WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE));
-//
-//		frame.pack();
-//		frame.setVisible(true);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		launch();
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Group root = new Group();
-		Scene scene = new Scene(root, WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE);
-		stage.setScene(scene);
-		stage.setResizable(false);
-
-		stage.sizeToScene();
-//		Canvas canvas = new Canvas(512, 512);
-		root.getChildren().add(new GamePanel(WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE));
-		stage.show();
+		GamePanel.getInstance().init(stage);
 	}
 }
+
+//public class Main extends Application {
+//
+//	public static void main(String[] args) {
+////		JFrame frame = new JFrame();
+////		frame.setResizable(false);
+////		frame.add(new GamePanel(WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE));
+////
+////		frame.pack();
+////		frame.setVisible(true);
+////		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		launch();
+//	}
+//
+//	@Override
+//	public void start(Stage stage) throws Exception {
+//		GamePanel.getInstance().init(stage);
+//
+//
+////		Group root = new Group();
+////		Scene scene = new Scene(root, WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE);
+////		stage.setScene(scene);
+////		stage.setResizable(false);
+////
+////		stage.sizeToScene();
+//////		Canvas canvas = new Canvas(512, 512);
+////		root.getChildren().add(new GamePanel(WIDTH * UNIT_SIZE, HEIGHT * UNIT_SIZE));
+////		stage.show();
+//	}
+//}
