89b98077fae82d4c888d6a1861e5c1c9e039c7f5
Zack Hu <scyzh6@nottingham.edu.cn>
2021-12-03 13:54:27 +0800

    feature: add complete game pop up scene
    Main relevant file added:
     - completeGamePopup.fxml

---
+++ BubbleBobble/src/main/java/com/ae2dms/Controller/Game/PopUp/GameCompletePopUpController.java
@@ -0,0 +1,11 @@
+package com.ae2dms.Controller.Game.PopUp;
+
+import javafx.scene.input.MouseEvent;
+
+public class GameCompletePopUpController {
+    public void clickSaveRecord(MouseEvent mouseEvent) {
+    }
+
+    public void clickBackToMenu(MouseEvent mouseEvent) {
+    }
+}
