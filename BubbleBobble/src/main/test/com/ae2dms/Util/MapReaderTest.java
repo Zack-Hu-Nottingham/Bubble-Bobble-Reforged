306922d17274123bedf7f5c58eb99b648047022b
Zack Hu <scyzh6@nottingham.edu.cn>
2021-12-09 12:19:38 +0800

    test: test the behavior of mapreader
    Test include:
     - read map one
     - read map two
     - read map three

---
+++ BubbleBobble/src/main/test/com/ae2dms/Util/MapReaderTest.java
@@ -0,0 +1,90 @@
+package com.ae2dms.Util;
+
+import com.ae2dms.GamePanel;
+import com.ae2dms.Main;
+import com.ae2dms.Model.GameObject.Sprite.Boss;
+import com.ae2dms.Model.GameObject.Sprite.Enemy;
+import com.ae2dms.Model.GameObject.Sprite.Hero;
+import com.ae2dms.Model.Scene.GameScene;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+
+
+class MapReaderTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        super.start(stage);
+        Main.stage = stage;
+
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root, 24 * 20, 24 * 30);
+        stage.setScene(scene);
+    }
+
+    @Test
+    void readMapOne() {
+        MapReader mapReader = MapReader.getInstance();
+        GamePanel gamePanel = new GamePanel();
+
+        GamePanel.level = new SimpleIntegerProperty(1);
+        GameScene gameScene = new GameScene();
+        mapReader.setGameScene(gameScene);
+
+        mapReader.readMap(1);
+        ArrayList<Enemy> enemies = gameScene.getEnemies();
+        ArrayList<Hero> heroes = gameScene.getHeroes();
+        ArrayList<Boss> bosses = gameScene.getBosses();
+
+        Assertions.assertEquals(enemies.size(), 3);
+        Assertions.assertNotNull(heroes.get(0));
+        Assertions.assertTrue(bosses.isEmpty());
+
+    }
+
+    @Test
+    void readMapTwo() {
+        MapReader mapReader = MapReader.getInstance();
+
+        GamePanel.level = new SimpleIntegerProperty(1);
+        GameScene gameScene = new GameScene();
+
+        mapReader.setGameScene(gameScene);
+
+        mapReader.readMap(2);
+        ArrayList<Enemy> enemies = gameScene.getEnemies();
+        ArrayList<Hero> heroes = gameScene.getHeroes();
+        ArrayList<Boss> bosses = gameScene.getBosses();
+
+        Assertions.assertEquals(enemies.size(), 4);
+        Assertions.assertNotNull(heroes.get(0));
+        Assertions.assertTrue(bosses.isEmpty());
+    }
+
+    @Test
+    void readMapThree() {
+        MapReader mapReader = MapReader.getInstance();
+
+        GamePanel.level = new SimpleIntegerProperty(1);
+        GameScene gameScene = new GameScene();
+        mapReader.setGameScene(gameScene);
+
+        mapReader.readMap(3);
+        ArrayList<Enemy> enemies = gameScene.getEnemies();
+        ArrayList<Hero> heroes = gameScene.getHeroes();
+        ArrayList<Boss> bosses = gameScene.getBosses();
+
+        Assertions.assertEquals(enemies.size(), 4);
+        Assertions.assertNotNull(heroes.get(0));
+        Assertions.assertEquals(bosses.size(), 1);
+    }
+
+}
